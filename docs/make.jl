c9fbc91821c897b84f605663f524be929dacff78
carlodev <carlobrunellibs@gmail.com>
2023-02-17 16:31:36 +0100

    new link

--- docs/make.jl
+++ docs/make.jl
@@ -1,11 +1,2 @@
-push!(LOAD_PATH,"../src/")
-using SyntheticEddyMethod
-using Documenter
-makedocs(
-         sitename = "SyntheticEddyMethod.jl",
-         modules  = [SyntheticEddyMethod],
-         pages=[
-                "Home" => "index.md"
-               ])
-deploydocs(;
-    repo="github.com/carlodev/SyntheticEddyMethod.jl",)
+using Documenter, SyntheticEddyMethod
+makedocs(sitename="SyntheticEddyMethod")
