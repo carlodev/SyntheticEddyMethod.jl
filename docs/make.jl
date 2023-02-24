1a3559a738127905124940c17f176ae719f19059
carlodev <carlobrunellibs@gmail.com>
2023-02-24 14:22:50 +0100

    update docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,2 +1,13 @@
 using SyntheticEddyMethod, Documenter
 makedocs(sitename="SyntheticEddyMethod")
+
+makedocs(
+    strict=true,
+    sitename = "SyntheticEddyMethod.jl",
+    modules = [PartitionedArrays],
+    pages = [
+        "Introduction" => "index.md",
+        "usage.md",
+        "exploring.md",
+    ],
+)
