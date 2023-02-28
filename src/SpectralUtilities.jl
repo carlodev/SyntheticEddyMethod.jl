a720a88821b7238061720e6ce0d1884973c33ff3
carlodev <carlobrunellibs@gmail.com>
2023-02-28 09:26:29 +0100

    rm

--- src/SpectralUtilities.jl
+++ src/SpectralUtilities.jl
@@ -9,10 +9,6 @@ function fft_from_signal(q::Vector{Float64},dt)
     PSD = real(fftshift(PSD))
     freqs = fftshift(fftfreq(nt,1/dt))
     idx = findall(x -> x>0, freqs)
+    
     return PSD[idx], freqs[idx]
 end
-
-psd, fre = fft_from_signal(rand(5), 0.001)
-
-psd
-fre
