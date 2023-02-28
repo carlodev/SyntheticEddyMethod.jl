7f2276794ce33c851127a1006e5a1cb072844c04
carlodev <carlobrunellibs@gmail.com>
2023-02-28 09:26:07 +0100

    specialize fft_from_signal

--- src/SpectralUtilities.jl
+++ src/SpectralUtilities.jl
@@ -1,6 +1,7 @@
 #Spectral Tools
 
-function fft_from_signal(q,dt)
+function fft_from_signal(q::Vector{Float64},dt)
+
     nt=length(q)
     fhat=fft(q)
     
@@ -8,6 +9,10 @@ function fft_from_signal(q,dt)
     PSD = real(fftshift(PSD))
     freqs = fftshift(fftfreq(nt,1/dt))
     idx = findall(x -> x>0, freqs)
-
     return PSD[idx], freqs[idx]
 end
+
+psd, fre = fft_from_signal(rand(5), 0.001)
+
+psd
+fre
