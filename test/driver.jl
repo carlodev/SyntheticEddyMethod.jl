0876b22ff867de58012c5dd7939909bf1527f64c
carlodev <carlobrunellibs@gmail.com>
2023-03-01 11:58:27 +0100

    more tests

--- test/driver.jl
+++ test/driver.jl
@@ -1,6 +1,6 @@
 
 using Statistics
-function driver_test(TI::Float64; Nt = 2000)
+function driver_test(TI::Float64; Nt = 2000, s_fun = tent_fun)
 σ = 0.1 #eddy dimensions, the same in all the directions
 b = 5.0
 a = 0.0
@@ -11,8 +11,22 @@ y = collect(a:0.1:b)
 z = collect(a:0.1:b)
 
 
-Vboxinfo = VirtualBox(y,z,σ)
-N = Vboxinfo.N
+Vboxinfo = VirtualBox(y,z,σ; shape_fun = s_fun)
+
+@test Vboxinfo.σ == [σ,σ,σ]
+@test Vboxinfo.X_end == σ
+@test Vboxinfo.X_start == -σ
+@test Vboxinfo.Y_end == b+σ
+@test Vboxinfo.Y_start == a-σ
+@test Vboxinfo.Z_end == b+σ
+@test Vboxinfo.Z_start == a-σ
+
+Vboxinfo.N = 100
+@test Vboxinfo.N == 100
+
+Vboxinfo = VirtualBox(y,z,σ; shape_fun = s_fun)
+
+
 dt = 0.01
 
 U₀ = 1.0 #Convective Velocity
@@ -27,13 +41,12 @@ Re_stress = [u_p 0.0 0.0;
             0.0 0.0 u_p]
 
 
-
-A = cholesky_decomposition(Re_stress)
 Eddies = initialize_eddies(N, σ, Vboxinfo)
 
 # Or you can use the wrapper function
-A, Eddies = initialize_eddies(U₀, TI, Vboxinfo)
+Re_stress_2, Eddies = initialize_eddies(U₀, TI, Vboxinfo)
 
+@test Re_stress == Re_stress_2
 
 #Computing the velocity in the middle of the VirtualBox domain
 vector_points = [[0.0, b/2, b/2]]
@@ -45,14 +58,16 @@ U = zeros(Nt, 3)
 
 
 for i = 1:1:Nt
-    U[i,:] = compute_fluct(vector_points, dt, Eddies, U₀, Vboxinfo, A)[1]
+    U[i,:] = compute_fluct(vector_points, dt, Eddies, U₀, Vboxinfo, Re_stress)[1]
 end
 
 #The deviation standard should approach the turbulence intensity
-s1 = Statistics.std(U[:,1])
-s2 = Statistics.std(U[:,2])
-s3 = Statistics.std(U[:,3])
+    s1 = Statistics.std(U[:,1])
+    s2 = Statistics.std(U[:,2])
+    s3 = Statistics.std(U[:,3])
+
 return mean([s1,s2,s3])
+
 end
 
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -6,4 +6,9 @@ using Plots
     @test isapprox(driver_test(0.1), 0.1; rtol =0.2)
     @test isapprox(driver_test(0.01), 0.01; rtol =0.2)
     @test isapprox(driver_test(0.05), 0.05; rtol =0.2)
+
+    @test isapprox(driver_test(0.01; s_fun = step_fun), 0.01; rtol =0.2)
+    @test isapprox(driver_test(0.01; s_fun = trunc_gauss_fun), 0.01; rtol =0.2)
+
+
 end    
