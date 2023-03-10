1d9bc9159129ee64b97af4e5ad56ef9a82091b6e
carlodev <carlobrunellibs@gmail.com>
2023-03-10 12:07:07 +0100

    add tests

--- test/read_reynoldsstress.jl
+++ test/read_reynoldsstress.jl
@@ -1,7 +1,10 @@
 using XLSX
  function read_re_test()
-    reynolds_stress_file = joinpath(@__DIR__,"Data","Re_ch.xlsx")
-    Re_from_file = get_reynolds_stress_from_file(reynolds_stress_file)
+   ftest = ["Re_ch.xlsx", "Re_stress.xlsx"]
+   dimstest = [(:Z, :Y), (:Y,)]
+   for (fname,dims) in zip(ftest,dimstest)
+    reynolds_stress_file = joinpath(@__DIR__,"Data",fname)
+    Re_from_file = get_reynolds_stress_from_file(reynolds_stress_file; dims = dims)
     @test typeof(Re_from_file) == Reynolds_stress_interpolator
 
     σ = 0.1 #eddy dimensions, the same in all the directions
@@ -16,13 +19,12 @@ using XLSX
     dt = 0.01
     U₀ = 1.0 #Convective Velocity
     Eddies = initialize_eddies(Vboxinfo)
-    vector_points = [[0.0, 1.2, 3.0], [0.0, 0.9, 4.0]]
+    vector_points = [[0.0, 0.2, 3.0], [0.0, 0.9, 4.0]]
     
     u_f = compute_fluct(vector_points, dt, Eddies, U₀, Vboxinfo, Re_from_file)
 
     @test typeof(u_f) == Vector{Vector{Float64}}
 
-
+   end
  end
-
 
